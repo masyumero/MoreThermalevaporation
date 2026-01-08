7974215e7a264826464ab9373c347899abb8cd11
A8 <161058439+A8T1N@users.noreply.github.com>
2026-01-08 21:50:48 +0900

    Ver 2.0 ClientRegistration 登録の最適化

--- src/main/java/morethermalevaporation/client/ClientRegistration.java
+++ src/main/java/morethermalevaporation/client/ClientRegistration.java
@@ -6,6 +6,7 @@ import morethermalevaporation.client.gui.GuiMoreThermalEvaporationController;
 import morethermalevaporation.client.render.tileentity.RenderMoreThermalEvaporationPlant;
 import morethermalevaporation.common.registries.MoreThermalEvaporationContainerTypes;
 import morethermalevaporation.common.registries.MoreThermalEvaporationTileEntityTypes;
+import morethermalevaporation.common.tier.MoreThermalEvaporationTier;
 import net.minecraft.core.registries.Registries;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
@@ -19,19 +20,17 @@ public class ClientRegistration {
 
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        event.registerBlockEntityRenderer(MoreThermalEvaporationTileEntityTypes.BASIC_THERMAL_EVAPORATION_CONTROLLER.get(), RenderMoreThermalEvaporationPlant::new);
-        event.registerBlockEntityRenderer(MoreThermalEvaporationTileEntityTypes.ADVANCED_THERMAL_EVAPORATION_CONTROLLER.get(), RenderMoreThermalEvaporationPlant::new);
-        event.registerBlockEntityRenderer(MoreThermalEvaporationTileEntityTypes.ELITE_THERMAL_EVAPORATION_CONTROLLER.get(), RenderMoreThermalEvaporationPlant::new);
-        event.registerBlockEntityRenderer(MoreThermalEvaporationTileEntityTypes.ULTIMATE_THERMAL_EVAPORATION_CONTROLLER.get(), RenderMoreThermalEvaporationPlant::new);
+        for (MoreThermalEvaporationTier tier : MoreThermalEvaporationTier.values()) {
+            event.registerBlockEntityRenderer(MoreThermalEvaporationTileEntityTypes.getTileEntityTypeController(tier).get(), (context) -> new RenderMoreThermalEvaporationPlant(tier, context));
+        }
     }
 
     @SubscribeEvent(priority = EventPriority.LOW)
     public static void registerContainers(RegisterEvent event) {
         event.register(Registries.MENU, helper -> {
-            ClientRegistrationUtil.registerScreen(MoreThermalEvaporationContainerTypes.BASIC_THERMAL_EVAPORATION_CONTROLLER, GuiMoreThermalEvaporationController::new);
-            ClientRegistrationUtil.registerScreen(MoreThermalEvaporationContainerTypes.ADVANCED_THERMAL_EVAPORATION_CONTROLLER, GuiMoreThermalEvaporationController::new);
-            ClientRegistrationUtil.registerScreen(MoreThermalEvaporationContainerTypes.ELITE_THERMAL_EVAPORATION_CONTROLLER, GuiMoreThermalEvaporationController::new);
-            ClientRegistrationUtil.registerScreen(MoreThermalEvaporationContainerTypes.ULTIMATE_THERMAL_EVAPORATION_CONTROLLER, GuiMoreThermalEvaporationController::new);
+            for (MoreThermalEvaporationTier tier : MoreThermalEvaporationTier.values()) {
+                ClientRegistrationUtil.registerScreen(MoreThermalEvaporationContainerTypes.getContainerTypes(tier), GuiMoreThermalEvaporationController::new);
+            }
         });
     }
 }
