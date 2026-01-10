95a319ec60bebd80b96034d2f1d4c2dab4f32166
A8 <161058439+A8T1N@users.noreply.github.com>
2026-01-10 17:11:17 +0900

    Ver 2.0 MoreThermalEvaporationConfig コンフィグクラスの追加

--- src/main/java/morethermalevaporation/common/config/MoreThermalEvaporationConfig.java
+++ src/main/java/morethermalevaporation/common/config/MoreThermalEvaporationConfig.java
@@ -1,90 +1,17 @@
 package morethermalevaporation.common.config;
 
-import morethermalevaporation.MoreThermalEvaporation;
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.common.Mod;
+import mekanism.common.config.MekanismConfigHelper;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModLoadingContext;
 
-@Mod.EventBusSubscriber(modid = MoreThermalEvaporation.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MoreThermalEvaporationConfig {
-    public static final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-    public static final ForgeConfigSpec configSpec;
+    public static final MoreThermalEvaporationPlantConfig config = new MoreThermalEvaporationPlantConfig();
 
-    public static final ForgeConfigSpec.IntValue BasicEvaporationOutputTankCapacity;
-    public static final ForgeConfigSpec.IntValue BasicEvaporationPlantHeight;
+    private MoreThermalEvaporationConfig() {
+    }
 
-    public static final ForgeConfigSpec.IntValue AdvancedEvaporationOutputTankCapacity;
-    public static final ForgeConfigSpec.IntValue AdvancedEvaporationPlantHeight;
-
-    public static final ForgeConfigSpec.IntValue EliteEvaporationOutputTankCapacity;
-    public static final ForgeConfigSpec.IntValue EliteEvaporationPlantHeight;
-
-    public static final ForgeConfigSpec.IntValue UltimateEvaporationOutputTankCapacity;
-    public static final ForgeConfigSpec.IntValue UltimateEvaporationPlantHeight;
-
-    public final static ForgeConfigSpec.BooleanValue RenderFluid;
-
-    static {
-        builder.comment("More Thermal Evaporation Settings");
-        builder.push("Tier");
-
-        // Basic
-        builder.push("Basic");
-        builder.comment("Settings for the Basic Tier");
-        BasicEvaporationOutputTankCapacity = builder
-                .comment("Amount of output fluid (mB) that the Basic Evaporation Plant can store.")
-                .defineInRange("BasicEvaporationOutputTankCapacity", 20000, 1, 2147483646);
-
-        BasicEvaporationPlantHeight = builder
-                .comment("Buildable Height (in blocks) for the Basic Evaporation Plant.")
-                .defineInRange("BasicEvaporationPlantHeight", 18, 18, 36);
-        builder.pop();
-
-        // Advanced
-        builder.push("Advanced");
-        builder.comment("Settings for the Advanced Tier");
-        AdvancedEvaporationOutputTankCapacity = builder
-                .comment("Amount of output fluid (mB) that the Advanced Evaporation Plant can store.")
-                .defineInRange("AdvancedEvaporationOutputTankCapacity", 80000, 1, 2147483646);
-
-        AdvancedEvaporationPlantHeight = builder
-                .comment("Buildable Height (in blocks) for the Advanced Evaporation Plant.")
-                .defineInRange("AdvancedEvaporationPlantHeight", 18, 18, 72);
-        builder.pop();
-
-        // Elite
-        builder.push("Elite");
-        builder.comment("Settings for the Elite Tier");
-        EliteEvaporationOutputTankCapacity = builder
-                .comment("Amount of output fluid (mB) that the Elite Evaporation Plant can store.")
-                .defineInRange("EliteEvaporationOutputTankCapacity", 640000, 1, 2147483646);
-
-        EliteEvaporationPlantHeight = builder
-                .comment("Buildable Height (in blocks) for the Elite Evaporation Plant.")
-                .defineInRange("EliteEvaporationPlantHeight", 18, 18, 144);
-        builder.pop();
-
-        // Ultimate
-        builder.push("Ultimate");
-        builder.comment("Settings for the Ultimate Tier");
-        UltimateEvaporationOutputTankCapacity = builder
-                .comment("Amount of output fluid (mB) that the Ultimate Evaporation Plant can store.")
-                .defineInRange("UltimateEvaporationOutputTankCapacity", 10240000, 1, 2147483646);
-
-        UltimateEvaporationPlantHeight = builder
-                .comment("Buildable Height (in blocks) for the Ultimate Evaporation Plant.")
-                .defineInRange("UltimateEvaporationPlantHeight", 18, 18, 288);
-        builder.pop();
-
-        builder.pop(); // Tier
-
-        builder.push("Render");
-        builder.comment("Settings for the Render fluid");
-        RenderFluid = builder
-                .comment("Render fluid inside of Thermal Evaporation Plants.")
-                .define("RenderFluid", true);
-        builder.pop(); // Render
-
-
-        configSpec = builder.build();
+    public static void registerConfig(ModLoadingContext modLoadingContext) {
+        ModContainer modContainer = modLoadingContext.getActiveContainer();
+        MekanismConfigHelper.registerConfig(modContainer, config);
     }
 }
---
+++ src/main/java/morethermalevaporation/common/config/MoreThermalEvaporationPlantConfig.java
@@ -0,0 +1,64 @@
+package morethermalevaporation.common.config;
+
+import mekanism.common.config.BaseMekanismConfig;
+import mekanism.common.config.value.CachedBooleanValue;
+import mekanism.common.config.value.CachedDoubleValue;
+import mekanism.common.config.value.CachedIntValue;
+import morethermalevaporation.common.tier.MoreThermalEvaporationTier;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.config.ModConfig;
+
+import java.util.Locale;
+
+public class MoreThermalEvaporationPlantConfig extends BaseMekanismConfig {
+    public static final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+    public final CachedBooleanValue RenderFluid;
+    public final ForgeConfigSpec configSpec;
+
+    MoreThermalEvaporationPlantConfig() {
+
+        addMoreThermalEvaporationCategory(builder);
+        builder.comment("Settings for the Render fluid");
+        RenderFluid = CachedBooleanValue.wrap(this, builder.comment("Render fluid inside of Thermal Evaporation Plants.").define("RenderFluid", true));
+        builder.pop(); // Render
+        configSpec = builder.build();
+    }
+
+    private void addMoreThermalEvaporationCategory(ForgeConfigSpec.Builder builder) {
+        builder.comment("More Thermal Evaporations Settings").push("more_thermal_evaporation");
+        for (MoreThermalEvaporationTier tier : MoreThermalEvaporationTier.values()) {
+            String tierName = tier.getBaseTier().getSimpleName();
+            CachedDoubleValue multiplierTempReference = CachedDoubleValue.wrap(this, builder.comment("Maximum " + "temperature capping the temperature multiplier for the " + tierName + "Thermal Evaporation Plant.")
+                    .defineInRange(tierName.toLowerCase(Locale.ROOT) + "ThermalEvaporationMultiplierTempCap",
+                            tier.getBaseMultiplierTemp(), 3000, 2147483646));
+
+            CachedIntValue heightReference = CachedIntValue.wrap(this, builder.comment("Buildable Height (in blocks) for the " + tierName + "Thermal Evaporation Plant.")
+                    .defineInRange(tierName.toLowerCase(Locale.ROOT) + "ThermalEvaporationHeight", tier.getBaseHeight(), 18, 2147483646));
+
+            CachedIntValue outputTankCapacityReference = CachedIntValue.wrap(this, builder.comment("Amount of output fluid (mB) that the " + tierName + "Thermal Evaporation Plant can store.")
+                    .defineInRange(tierName.toLowerCase(Locale.ROOT) + "ThermalEvaporationOutputTankCapacity", tier.getBaseOutputTankCapacity(), 1, 2147483646));
+
+            tier.setConfigReference(multiplierTempReference, heightReference, outputTankCapacityReference);
+        }
+    }
+
+    @Override
+    public String getFileName() {
+        return "more-thermal-evaporations";
+    }
+
+    @Override
+    public ForgeConfigSpec getConfigSpec() {
+        return configSpec;
+    }
+
+    @Override
+    public ModConfig.Type getConfigType() {
+        return ModConfig.Type.COMMON;
+    }
+
+    @Override
+    public boolean addToContainer() {
+        return false;
+    }
+}
